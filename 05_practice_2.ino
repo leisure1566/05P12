749a306b0efd4f68b474e9f03472e3a4c3288086
leisure1566 <leisure1566@gmail.com>
2024-09-21 20:20:25 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,21 @@
+int ledPin = 7;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  }
+
+  digitalWrite(ledPin, HIGH);
+}
+
+void loop() {
+  // 아무 작업도 하지 않음
+}
